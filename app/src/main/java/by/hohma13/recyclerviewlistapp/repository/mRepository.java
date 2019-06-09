ddef3a6fbd8b041af20fa720b693e1b04065ddfa
hohma <hohma13@gmail.com>
2019-06-09 11:37:57 +0300

    create base repository

---
+++ app/src/main/java/by/hohma13/recyclerviewlistapp/repository/mRepository.java
@@ -0,0 +1,34 @@
+package by.hohma13.recyclerviewlistapp.repository;
+
+import java.util.ArrayList;
+
+import by.hohma13.recyclerviewlistapp.minterface.mRepositoryInterface;
+
+public class mRepository implements mRepositoryInterface {
+
+
+    @Override
+    public void getDB() {
+
+    }
+
+    @Override
+    public void setDB() {
+
+    }
+
+    @Override
+    public ArrayList<Integer> getDBList() {
+        return null;
+    }
+
+    @Override
+    public int addItem(int itemNew) {
+        return 0;
+    }
+
+    @Override
+    public void deleteAllTable() {
+
+    }
+}
