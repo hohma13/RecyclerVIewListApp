4b2e5a61fd8ca33c6c6531da40175cfcd97223bc
hohma <hohma13@gmail.com>
2019-06-09 11:40:39 +0300

    create base presenter

---
+++ app/src/main/java/by/hohma13/recyclerviewlistapp/mpresenter/mPresenter.java
@@ -0,0 +1,32 @@
+package by.hohma13.recyclerviewlistapp.mpresenter;
+
+import java.util.ArrayList;
+
+import by.hohma13.recyclerviewlistapp.minterface.mPresenterInterface;
+
+public class mPresenter implements mPresenterInterface {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void addListItem(int itemId) {
+
+    }
+
+    @Override
+    public void deleteListItem(int itemId) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public void listRestore(ArrayList<Integer> integerArrayList) {
+
+    }
+}
