9a20d293340d20354c0b3f56fc23043347c44994
hohma <hohma13@gmail.com>
2019-06-09 20:27:33 +0300

    create all method

--- app/src/main/java/by/hohma13/recyclerviewlistapp/mpresenter/MPresenter.java
+++ app/src/main/java/by/hohma13/recyclerviewlistapp/mpresenter/MPresenter.java
@@ -2,31 +2,42 @@ package by.hohma13.recyclerviewlistapp.mpresenter;
 
 import java.util.ArrayList;
 
+
 import by.hohma13.recyclerviewlistapp.minterface.mPresenterInterface;
+import by.hohma13.recyclerviewlistapp.minterface.mRepositoryInterface;
+import by.hohma13.recyclerviewlistapp.minterface.mViewInterface;
+import by.hohma13.recyclerviewlistapp.repository.MRepository;
 
 public class MPresenter implements mPresenterInterface {
+
+    private mViewInterface mView;
+    private mRepositoryInterface mRepositoryInterface;
+    private ArrayList<Integer> list;
+
+    public MPresenter(mViewInterface mView) {
+        this.mView = mView;
+        this.mRepositoryInterface = new MRepository();
+    }
+
     @Override
     public void create() {
-
+        list = mRepositoryInterface.getDB();
+        mView.showList(list);
     }
 
     @Override
     public void addListItem(int itemId) {
-
+        mView.showList(mRepositoryInterface.addItem(itemId));
     }
 
     @Override
     public void deleteListItem(int itemId) {
-
+        mView.showList(mRepositoryInterface.deleteItem(itemId));
     }
 
-    @Override
-    public void deleteAll() {
-
-    }
 
     @Override
-    public void listRestore(ArrayList<Integer> integerArrayList) {
-
+    public void listRestore(ArrayList<Integer> restoreList) {
+        mView.showList(restoreList);
     }
 }
